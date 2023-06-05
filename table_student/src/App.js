92f3dd4c7570947026ed191440328c13f1b2678d
tienhung79 <ngotienhung2000@gmail.com>
2023-06-05 15:27:40 +0700

    upp

--- table_student/src/App.js
+++ table_student/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {students} from './newFolder/data.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <table className='container' >
+      <>
+      <h1>Student</h1>
+      <tr>
+        <td>Company</td>
+        <td>contact</td>
+        <td>Country</td>
+      </tr>
+    {students.map(student => (
+      <tr>
+        <td>{student.company}</td>
+        <td>{student.contact}</td>
+        <td>{student.country}</td>
+      </tr>
+    ))}
+    </>
+    </table>
+)}
 
 export default App;
